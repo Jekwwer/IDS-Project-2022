f90720d4d31eb1bdfbca25e970f13647208acd43
Evgenii Shiliaev <xshili00@stud.fit.vutbr.cz>
2022-04-08 19:27:53 +0200

    Code style changes

--- xshili00_xbrazd22.sql
+++ xshili00_xbrazd22.sql
@@ -197,35 +197,35 @@ INSERT INTO Nahravka
 INSERT INTO Kazeta (id_nahravky, sazba_vypujceni, porizovaci_cena, datum_zarazeni)
     SELECT id_nahravky, 100, 180, TO_DATE('1.2.2010')
     FROM Nahravka
-    WHERE nazev='Forrest Gump';
+    WHERE nazev = 'Forrest Gump';
 INSERT INTO Kazeta (id_nahravky, sazba_vypujceni, porizovaci_cena, datum_zarazeni)
     SELECT id_nahravky, 100, 180, TO_DATE('1.2.2010')
     FROM Nahravka
-    WHERE nazev='Forrest Gump';
+    WHERE nazev = 'Forrest Gump';
 INSERT INTO Kazeta (id_nahravky, sazba_vypujceni, porizovaci_cena, datum_zarazeni)
     SELECT id_nahravky, 60, 150, TO_DATE('1.6.2015')
     FROM Nahravka
-    WHERE nazev='Sociální síť';
+    WHERE nazev = 'Sociální síť';
 INSERT INTO Kazeta (id_nahravky, sazba_vypujceni, porizovaci_cena, datum_zarazeni)
     SELECT id_nahravky, 60, 150, TO_DATE('1.6.2015')
     FROM Nahravka
-    WHERE nazev='Sociální síť';
+    WHERE nazev = 'Sociální síť';
 INSERT INTO Kazeta (id_nahravky, sazba_vypujceni, stav, porizovaci_cena, datum_zarazeni, datum_vyrazeni)
     SELECT id_nahravky, 60, 'Vyřazeno', 150, TO_DATE('1.6.2015'), TO_DATE('21.3.2017')
     FROM Nahravka
-    WHERE nazev='Sociální síť';
+    WHERE nazev = 'Sociální síť';
 INSERT INTO Kazeta (id_nahravky, sazba_vypujceni, porizovaci_cena, datum_zarazeni)
     SELECT id_nahravky, 110, 190, TO_DATE('1.2.2020')
     FROM Nahravka
-    WHERE nazev='Jexi: Láska z mobilu';
+    WHERE nazev = 'Jexi: Láska z mobilu';
 INSERT INTO Kazeta (id_nahravky, sazba_vypujceni, porizovaci_cena, datum_zarazeni)
     SELECT id_nahravky, 110, 190, TO_DATE('1.2.2020')
     FROM Nahravka
-    WHERE nazev='Jexi: Láska z mobilu';
+    WHERE nazev = 'Jexi: Láska z mobilu';
 INSERT INTO Kazeta (id_nahravky, sazba_vypujceni, porizovaci_cena, datum_zarazeni)
     SELECT id_nahravky, 110, 190, TO_DATE('1.2.2020')
     FROM Nahravka
-    WHERE nazev='Jexi: Láska z mobilu';
+    WHERE nazev = 'Jexi: Láska z mobilu';
 
 INSERT INTO Zanr
     VALUES('Drama');
@@ -263,19 +263,19 @@ INSERT INTO Nahravka_Zanru
 
 INSERT INTO Zakaznik
     VALUES(DEFAULT, 'Evgenii', 'Shiliaev', TO_DATE('01.01.2001'), '420000000000', 'asdf-moje-posta@mail.com',
-           'Božetěchova 1/2', 'Brno', '612 00');
+            'Božetěchova 1/2', 'Brno', '612 00');
 INSERT INTO Zakaznik
     VALUES(DEFAULT, 'Pavel', 'Novák', TO_DATE('24.12.1991'), '420123123123', 'pavel.novak@neznam.cz',
-           'Krátká 432/2', 'Adamov', '679 04');
+            'Krátká 432/2', 'Adamov', '679 04');
 INSERT INTO Zakaznik
     VALUES(DEFAULT, 'Eva', 'Svobodová', TO_DATE('28.07.1995'), '420147258369', 'svobodova33@kmail.cz',
-           'Netroufalky 770', 'Brno', '625 00');
+            'Netroufalky 770', 'Brno', '625 00');
 INSERT INTO Zakaznik
     VALUES(DEFAULT, 'Jiří', 'Černý', TO_DATE('31.05.2007'), '420741258963', 'cerny.jiri05@inlook.com',
-           'Lidická 1875/40', 'Brno', '605 00');
+            'Lidická 1875/40', 'Brno', '605 00');
 INSERT INTO Zakaznik
     VALUES(DEFAULT, 'Jan', 'Dvořák', TO_DATE('10.02.1999'), '420159753648', 'dvorak.honz4@protectedmail.com',
-           'Lidická 1875/40', 'Brno', '602 00');
+            'Lidická 1875/40', 'Brno', '602 00');
 
 INSERT INTO Zamestnanec
     VALUES(DEFAULT, 'Ladislav', 'Marek', TO_DATE('16.08.1985'), '420658495327', 'lada.marek@eznam.cz',
@@ -301,13 +301,13 @@ INSERT INTO Rezervace (id_zakaznika, id_nahravky, datum)
 INSERT INTO Rezervace (id_zakaznika, id_nahravky, datum)
     SELECT id_zakaznika, id_nahravky, TO_DATE('2.4.2022')
     FROM Zakaznik CROSS JOIN Nahravka
-    WHERE jmeno='Eva' AND prijmeni = 'Svobodová' AND nazev='Sociální síť';
+    WHERE jmeno = 'Eva' AND prijmeni = 'Svobodová' AND nazev = 'Sociální síť';
 
 INSERT INTO Vypujcka (datum_do, cena, id_rezervace, id_nahravky, id_kazety, id_zakaznika, vydano_zamestnancem)
     SELECT TO_DATE('5.4.2022'), sazba_vypujceni*(TO_DATE('5.4.2022') - TO_DATE(CURRENT_DATE)), id_rezervace, Kazeta.id_nahravky, id_kazety, id_zakaznika, id_zamestnance
     FROM Rezervace CROSS JOIN Kazeta CROSS JOIN Zamestnanec
-    WHERE id_rezervace=1 AND Rezervace.id_nahravky=Kazeta.id_nahravky AND Kazeta.stav='Skladem' AND
-      jmeno='Jan' AND prijmeni = 'Culek' AND ROWNUM <= 1;
+    WHERE id_rezervace = 1 AND Rezervace.id_nahravky = Kazeta.id_nahravky AND Kazeta.stav = 'Skladem' AND
+        jmeno = 'Jan' AND prijmeni = 'Culek' AND ROWNUM <= 1;
 
 UPDATE Rezervace
     SET stav = 'Vyrizeno'
@@ -323,8 +323,8 @@ UPDATE Kazeta
 INSERT INTO Vypujcka (datum_do, cena, id_rezervace, id_nahravky, id_kazety, id_zakaznika, vydano_zamestnancem)
     SELECT TO_DATE('5.4.2022'), sazba_vypujceni*(TO_DATE('5.4.2022') - TO_DATE(CURRENT_DATE)), id_rezervace, Kazeta.id_nahravky, id_kazety, id_zakaznika, id_zamestnance
     FROM Rezervace CROSS JOIN Kazeta CROSS JOIN Zamestnanec
-    WHERE id_rezervace=2 AND Rezervace.id_nahravky=Kazeta.id_nahravky AND Kazeta.stav='Skladem' AND
-      jmeno='Jan' AND prijmeni = 'Culek' AND ROWNUM <= 1;
+    WHERE id_rezervace = 2 AND Rezervace.id_nahravky = Kazeta.id_nahravky AND Kazeta.stav = 'Skladem' AND
+        jmeno = 'Jan' AND prijmeni = 'Culek' AND ROWNUM <= 1;
 
 UPDATE Rezervace
     SET stav = 'Vyřizeno'
@@ -335,21 +335,21 @@ UPDATE Kazeta
     WHERE id_kazety IN (
         SELECT id_kazety
         FROM Vypujcka
-        WHERE id_rezervace =2);
+        WHERE id_rezervace = 2);
 
 INSERT INTO Vypujcka (datum_od, datum_do, cena, id_nahravky, id_kazety, id_zakaznika, vydano_zamestnancem)
     SELECT TO_DATE('2.4.2022'), TO_DATE('7.4.2022'), sazba_vypujceni*(TO_DATE('7.4.2022') - TO_DATE('2.4.2022')),
-           Kazeta.id_nahravky, id_kazety, id_zakaznika, id_zamestnance
+        Kazeta.id_nahravky, id_kazety, id_zakaznika, id_zamestnance
     FROM Nahravka CROSS JOIN Kazeta CROSS JOIN Zamestnanec CROSS JOIN ZAKAZNIK
-    WHERE nazev = 'Jexi: Láska z mobilu' AND Kazeta.stav='Skladem' AND Zamestnanec.jmeno='Jan' AND
-          Zamestnanec.prijmeni = 'Culek' AND Zakaznik.jmeno = 'Jiří' AND Zakaznik.prijmeni='Černý' AND ROWNUM <= 1;
+    WHERE nazev = 'Jexi: Láska z mobilu' AND Kazeta.stav = 'Skladem' AND Zamestnanec.jmeno = 'Jan' AND
+        Zamestnanec.prijmeni = 'Culek' AND Zakaznik.jmeno = 'Jiří' AND Zakaznik.prijmeni = 'Černý' AND ROWNUM <= 1;
 
 UPDATE Kazeta
     SET stav = 'Vypůjčeno'
     WHERE id_kazety IN (
         SELECT id_kazety
         FROM Vypujcka NATURAL JOIN Zakaznik
-        WHERE datum_do = TO_DATE('7.4.2022') AND jmeno = 'Jiří' AND prijmeni='Černý');
+        WHERE datum_do = TO_DATE('7.4.2022') AND jmeno = 'Jiří' AND prijmeni = 'Černý');
 
 SELECT * FROM Zakaznik;
 SELECT * FROM Zamestnanec;
@@ -367,11 +367,11 @@ SELECT * FROM Nahravka_Zanru;
 /* Nahravky zanru drama */
 SELECT nazev
     FROM Nahravka NATURAL JOIN NAHRAVKA_ZANRU
-    WHERE zanr='Drama';
+    WHERE zanr = 'Drama';
 
 /* Nahravky v anglickem jazyce zneni */
 SELECT nazev
     FROM Nahravka
-    WHERE jazyk_zneni='Angličtina';
+    WHERE jazyk_zneni = 'Angličtina';
 
 /* End of xshili00_xbrazd22.sql */
